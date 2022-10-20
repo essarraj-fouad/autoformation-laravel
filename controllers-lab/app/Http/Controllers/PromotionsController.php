8b9deda4183025e6d8d9a0c67219b4ee3db705aa
= <=>
2022-10-20 11:47:14 +0100

    Les bases - controllers

---
+++ controllers-lab/app/Http/Controllers/PromotionsController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PromotionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return "<h1>Liste des promotions</h1>";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return "<h1>Ajouter une promotion</h1>";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return "<h1>Créer un nouveau promotion</h1>";
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return "<h1>Afficher la promotion numéro ".$id."</h1>";
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return "<h1>Edition la promotion numéro ".$id."</h1>";
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return "<h1>Mise à jour de la promotion numéro ".$id."</h1>";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- controllers-lab/routes/web.php
+++ controllers-lab/routes/web.php
@@ -3,35 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route par défaut
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/bonjour', function () {
-    return 'Bonjour ESSARRAJ Fouad';
-});
-
+use App\Http\Controllers\PromotionsController;
 
 Route::get('/', [HomeController::class, 'index']);
 
 
-Route::get('/produit/{id}', function ($id) {
-    return 'Affichage du produit numéro '.$id;
-});
 
-Route::get('/produit/{param1}/commentaire/{param2}', function ($produitId, $commentaireId) {
-    return 'Le produit numéro '.$produitId. 'avec la commentaire numéro '.$commentaireId;
-});
+ 
+Route::resource('promotions', PromotionsController::class);
+
